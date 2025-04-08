21d3cc4211d245ff42d71651b34a2140d7e27024
Esraa Alsubhi <162142964+ovzev@users.noreply.github.com>
2025-04-08 19:09:48 +0400

    main.dart

--- main.dart
+++ main.dart
@@ -1,1 +1,16 @@
+import 'package:flutter/material.dart';
+
+// Enum for Product Categories
+enum ProductCategory { Electronics, Groceries, Clothing }
+
+// Product Class
+class Product {
+  String id;
+  String name;
+  ProductCategory category;
+  double price;
+  int quantity;
+
+  Product(this.id, this.name, this.category, this.price, this.quantity);
+}
 
